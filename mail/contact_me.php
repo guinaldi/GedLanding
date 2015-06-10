47e9babe803618f0ae22f4e7a7025de52d4a8f89
unknown <guinaldi@hotmail.com>
2015-06-10 02:34:22 -0300

    Email configurado

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$to = 'contato@cloudmed.io'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "GEDWEB Landing Page:  $name";
+$email_body = "Uma nova mensagem foi gerada na Landing Page do GEDWEB.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTel: $phone\n\nMensagem:\n$message";
+$headers = "De: noreply@cloudmed.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responder Para: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
